b9a58805f708fcecbc8ea3b12d8abf775bbb976f
Yuki <yksk171118@gmail.com>
2023-02-23 15:48:36 +0900

    creat file:command.py

---
+++ src/command.py
@@ -0,0 +1,10 @@
+
+topCmdList = ['config','get','exit']
+configSubCmdList = ['save','print','help']
+
+def switchCMD(cmdStr = str):
+    cmd = cmdStr.split()
+    pass
+
+def switchConfigSubCMD(cmdStr = str):
+    pass
